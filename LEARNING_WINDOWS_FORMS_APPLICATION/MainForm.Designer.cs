d0860c27af99b774ab993f03e6b6d8493f6a8805
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-11-30 10:43:55 +0330

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.textBox11 = new System.Windows.Forms.TextBox();
+			this.textBox12 = new System.Windows.Forms.TextBox();
+			this.textBox13 = new System.Windows.Forms.TextBox();
+			this.textBox14 = new System.Windows.Forms.TextBox();
+			this.textBox15 = new System.Windows.Forms.TextBox();
+			this.textBox16 = new System.Windows.Forms.TextBox();
+			this.textBox17 = new System.Windows.Forms.TextBox();
+			this.textBox18 = new System.Windows.Forms.TextBox();
+			this.textBox19 = new System.Windows.Forms.TextBox();
+			this.textBox20 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(150, 23);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Hello, World!";
+			this.textBox1.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox1.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 41);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(150, 23);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "Hello, World!";
+			this.textBox2.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox2.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(12, 70);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(150, 23);
+			this.textBox3.TabIndex = 2;
+			this.textBox3.Text = "Hello, World!";
+			this.textBox3.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox3.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(12, 99);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(150, 23);
+			this.textBox4.TabIndex = 3;
+			this.textBox4.Text = "Hello, World!";
+			this.textBox4.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox4.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(12, 128);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(150, 23);
+			this.textBox5.TabIndex = 4;
+			this.textBox5.Text = "Hello, World!";
+			this.textBox5.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox5.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(12, 157);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(150, 23);
+			this.textBox6.TabIndex = 5;
+			this.textBox6.Text = "Hello, World!";
+			this.textBox6.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox6.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(12, 186);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(150, 23);
+			this.textBox7.TabIndex = 6;
+			this.textBox7.Text = "Hello, World!";
+			this.textBox7.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox7.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(12, 215);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(150, 23);
+			this.textBox8.TabIndex = 7;
+			this.textBox8.Text = "Hello, World!";
+			this.textBox8.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox8.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(12, 244);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(150, 23);
+			this.textBox9.TabIndex = 8;
+			this.textBox9.Text = "Hello, World!";
+			this.textBox9.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox9.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(12, 273);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.Size = new System.Drawing.Size(150, 23);
+			this.textBox10.TabIndex = 9;
+			this.textBox10.Text = "Hello, World!";
+			this.textBox10.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox10.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox11
+			// 
+			this.textBox11.Location = new System.Drawing.Point(168, 273);
+			this.textBox11.Name = "textBox11";
+			this.textBox11.Size = new System.Drawing.Size(150, 23);
+			this.textBox11.TabIndex = 19;
+			this.textBox11.Text = "Hello, World!";
+			this.textBox11.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox11.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox12
+			// 
+			this.textBox12.Location = new System.Drawing.Point(168, 244);
+			this.textBox12.Name = "textBox12";
+			this.textBox12.Size = new System.Drawing.Size(150, 23);
+			this.textBox12.TabIndex = 18;
+			this.textBox12.Text = "Hello, World!";
+			this.textBox12.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox12.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox13
+			// 
+			this.textBox13.Location = new System.Drawing.Point(168, 215);
+			this.textBox13.Name = "textBox13";
+			this.textBox13.Size = new System.Drawing.Size(150, 23);
+			this.textBox13.TabIndex = 17;
+			this.textBox13.Text = "Hello, World!";
+			this.textBox13.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox13.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox14
+			// 
+			this.textBox14.Location = new System.Drawing.Point(168, 186);
+			this.textBox14.Name = "textBox14";
+			this.textBox14.Size = new System.Drawing.Size(150, 23);
+			this.textBox14.TabIndex = 16;
+			this.textBox14.Text = "Hello, World!";
+			this.textBox14.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox14.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox15
+			// 
+			this.textBox15.Location = new System.Drawing.Point(168, 157);
+			this.textBox15.Name = "textBox15";
+			this.textBox15.Size = new System.Drawing.Size(150, 23);
+			this.textBox15.TabIndex = 15;
+			this.textBox15.Text = "Hello, World!";
+			this.textBox15.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox15.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox16
+			// 
+			this.textBox16.Location = new System.Drawing.Point(168, 128);
+			this.textBox16.Name = "textBox16";
+			this.textBox16.Size = new System.Drawing.Size(150, 23);
+			this.textBox16.TabIndex = 14;
+			this.textBox16.Text = "Hello, World!";
+			this.textBox16.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox16.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox17
+			// 
+			this.textBox17.Location = new System.Drawing.Point(168, 99);
+			this.textBox17.Name = "textBox17";
+			this.textBox17.Size = new System.Drawing.Size(150, 23);
+			this.textBox17.TabIndex = 13;
+			this.textBox17.Text = "Hello, World!";
+			this.textBox17.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox17.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox18
+			// 
+			this.textBox18.Location = new System.Drawing.Point(168, 70);
+			this.textBox18.Name = "textBox18";
+			this.textBox18.Size = new System.Drawing.Size(150, 23);
+			this.textBox18.TabIndex = 12;
+			this.textBox18.Text = "Hello, World!";
+			this.textBox18.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox18.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox19
+			// 
+			this.textBox19.Location = new System.Drawing.Point(168, 41);
+			this.textBox19.Name = "textBox19";
+			this.textBox19.Size = new System.Drawing.Size(150, 23);
+			this.textBox19.TabIndex = 11;
+			this.textBox19.Text = "Hello, World!";
+			this.textBox19.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox19.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// textBox20
+			// 
+			this.textBox20.Location = new System.Drawing.Point(168, 12);
+			this.textBox20.Name = "textBox20";
+			this.textBox20.Size = new System.Drawing.Size(150, 23);
+			this.textBox20.TabIndex = 10;
+			this.textBox20.Text = "Hello, World!";
+			this.textBox20.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.textBox20.Leave += new System.EventHandler(this.TextBox_Leave);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SteelBlue;
+			this.ClientSize = new System.Drawing.Size(333, 314);
+			this.Controls.Add(this.textBox11);
+			this.Controls.Add(this.textBox12);
+			this.Controls.Add(this.textBox13);
+			this.Controls.Add(this.textBox14);
+			this.Controls.Add(this.textBox15);
+			this.Controls.Add(this.textBox16);
+			this.Controls.Add(this.textBox17);
+			this.Controls.Add(this.textBox18);
+			this.Controls.Add(this.textBox19);
+			this.Controls.Add(this.textBox20);
+			this.Controls.Add(this.textBox10);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.TextBox textBox11;
+		private System.Windows.Forms.TextBox textBox12;
+		private System.Windows.Forms.TextBox textBox13;
+		private System.Windows.Forms.TextBox textBox14;
+		private System.Windows.Forms.TextBox textBox15;
+		private System.Windows.Forms.TextBox textBox16;
+		private System.Windows.Forms.TextBox textBox17;
+		private System.Windows.Forms.TextBox textBox18;
+		private System.Windows.Forms.TextBox textBox19;
+		private System.Windows.Forms.TextBox textBox20;
+	}
+}
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,41 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+
+		}
+
+		private void TextBox_Enter(object sender, System.EventArgs e)
+		{
+			// کادرمحاوره‌ای جاری که وارد آن شده‌ایم
+			System.Windows.Forms.TextBox
+				currentTextBox = sender as System.Windows.Forms.TextBox;
+
+			if (currentTextBox != null)
+			{
+				currentTextBox.Font =
+					new System.Drawing.Font(currentTextBox.Font, System.Drawing.FontStyle.Bold);
+			}
+		}
+
+		private void TextBox_Leave(object sender, System.EventArgs e)
+		{
+			// کادر محاوره‌ای جاری که از آن خارج شده‌ایم
+			System.Windows.Forms.TextBox
+				currentTextBox = sender as System.Windows.Forms.TextBox;
+
+			if (currentTextBox != null)
+			{
+				currentTextBox.Font =
+					new System.Drawing.Font(currentTextBox.Font, System.Drawing.FontStyle.Regular);
+			}
+		}
+	}
+}
